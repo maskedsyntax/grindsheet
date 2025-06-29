b8a3eb8e1f725e2d88460fe8921d827b7aa875a0
maskedsyntax <aftaab2507@gmail.com>
2025-06-29 16:45:45 +0530

    update database setup to use render's postgres

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,7 +2,18 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from app.core.config import settings
 
-engine = create_engine(settings.DATABASE_URL, connect_args={"check_same_thread": False})
+db_url = settings.DATABASE_URL
+if db_url.startswith("sqlite"):
+    engine = create_engine(
+        settings.DATABASE_URL, connect_args={"check_same_thread": False}
+    )
+else:
+    engine = create_engine(db_url)
+
+from app.models import Base
+
+Base.metadata.create_all(bind=engine)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
